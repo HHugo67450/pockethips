fd6040a24f7499215ce2f790a4146e233cce5173
Hugo <hugo.horlacher@gmail.com>
2025-12-27 22:23:26 +0100

    Mettre à jour le total des résultats quand on affiche les favoris

--- lib/notifiers/hips/hips_total_notifier.dart
+++ lib/notifiers/hips/hips_total_notifier.dart
@@ -1,60 +1,21 @@
 import 'dart:async';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:pocket_hips/notifiers/filters/filters_notifier.dart';
 
-import '../../../repository/hips_repository.dart';
-import 'hips_registry_notifier.dart';
+import 'hips_list_notifier.dart';
+
 
 class HipsTotalNotifier extends AsyncNotifier<int> {
 
   @override
   FutureOr<int> build() async {
-    final hipsRepository = ref.watch(hipsRepositoryProvider);
+    final filteredHipsListAsync = ref.watch(filteredHipsListProvider);
 
-    final selectedProviders = ref.watch(filtersProvider
-        .select((filtersState) => filtersState.selectedProviders));
-    final selectedContentTypes = ref.watch(filtersProvider
-        .select((filtersState) => filtersState.contentType));
-    final selectedColors = ref.watch(filtersProvider
-        .select((filtersState) => filtersState.colorSelected));
-    final yearRange = ref.watch(filtersProvider
-        .select((filtersState) => filtersState.yearRange));
-    final searchQuery = ref.watch(filtersProvider
-        .select((filtersState) => filtersState.searchQuery));
-
-    final hipsRegistryGroups = ref.watch(hipsRegistryGroupsProvider)
-        .value ?? [];
-
-    if (selectedProviders.isEmpty) {
-      return 0;
-    }
-
-    int totalHips = 0;
-    final futures = <Future<int?>>[];
-
-    for (final providerName in selectedProviders) {
-      final group = hipsRegistryGroups.firstWhere(
-        (group) => group.displayName == providerName,
-      );
-      for (final registry in group.hipsRegistries) {
-        futures.add(hipsRepository.getHipsTotal(
-          registry.url,
-          selectedContentTypes,
-          selectedColors,
-          yearRange!,
-          searchQuery,
-        ));
-      }
-    }
-
-    final results = await Future.wait(futures);
-    for (final result in results) {
-      if (result != null) {
-        totalHips += result;
-      }
-    }
-    return totalHips;
+    return filteredHipsListAsync.when(
+      data: (hipsDetails) => hipsDetails.length,
+      loading: () => 0,
+      error: (e, st) => 0,
+    );
   }
 }
 
